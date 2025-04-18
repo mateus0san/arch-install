04f66ffb12d902f3b3c4031731104addc932efa4
mateus0san <mateus0santiago@proton.me>
2025-04-18 13:26:46 -0300

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -1,30 +1,21 @@
 #connect to network
-echo 'connect to network' && sleep 2
+echo 'connect to network' || exit
 iwctl
 
 #part disk
-echo 'part disk' && sleep 2
+echo 'part disk' || exit
 cfdisk
 
 #format disks
 echo 'format disks' && sleep 2
-mkfs.ext4 /dev/sda3 && mkswap /dev/sda2 && mkfs.fat -F 32 /dev/sda1 && echo "Ok(format disks)" && sleep 2
+mkfs.ext4 /dev/sda3 && mkswap /dev/sda2 && mkfs.fat -F 32 /dev/sda1 && echo "Ok(format disks)" || exit
 
 #mount disks
 echo 'mount' && sleep 2
-mount /dev/sda3 /mnt && mout --mkdir /dev/sda1 /mnt/boot && swapon /dev/sda2 && echo "Ok(mount)" && sleep 2
+mount /dev/sda3 /mnt && mout --mkdir /dev/sda1 /mnt/boot && swapon /dev/sda2 && echo "Ok(mount)" || exit
 
 #Installing packages
 echo 'installing packages' && sleep 2
-pacstrap -K /mnt base linux linux-firmware vim intel-ucode sof-firmware networkmanager man-db man-pages texinfo && echo 'Ok(installing packages)' && sleep 2
+pacstrap -K /mnt base linux linux-firmware vim intel-ucode sof-firmware networkmanager man-db man-pages texinfo && echo 'Ok(installing packages)' || exit
 
-genfstab -U /mnt >> /mnt/etc/fstab && arch-chroot /mnt && echo 'everything is okay...' && echo sleep 2
-
-echo book550 >> /etc/hostname && systemctl enable NetworkManager && vim /etc/hosts && echo 'doing well...' && sleep 2
-
-echo 'type your root pass'
-passwd
-
-pacman -S grub efibootmgr && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --removable && grub-mkconfig -o /boot/grub/grub.cfg && echo 'ok...' && sleep 2
-
-exit && umount -R /mnt && reboot
+genfstab -U /mnt >> /mnt/etc/fstab && arch-chroot /mnt && echo 'everything is okay...' || exit
